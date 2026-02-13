e9ba8e21a4b9acebe33dfcf6abe208dabc59860f
Narayanaswamy-m <venkatanarayanamedikonda@gmail.com>
2026-02-13 16:18:48 +0530

    changed something

--- timestamp.sh
+++ timestamp.sh
@@ -4,5 +4,5 @@ Start_time=($date+%s);
 echo "script start time $Start_time"
 Sleep 10
 End_time=($date+%s);
-Total_time=((Start_time-End_time));
+Total_time=(($Start_time-$End_time));
 echo " script executed totaltime in seconds $Total_time"
