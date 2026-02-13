820195a259e26843a0637483f3503ded15f60265
Narayanaswamy-m <venkatanarayanamedikonda@gmail.com>
2026-02-13 18:04:11 +0530

    changed something

--- specialvariable.sh
+++ specialvariable.sh
@@ -5,6 +5,6 @@ echo "script nme $0"
 echo "Present working directory $PWD"
 echo "present user $USER"
 echo "PId Number $$"
-sleep 100
+sleep 100 &
 echo "background process id :$!"
 echo "all variables to scripts :$*"
