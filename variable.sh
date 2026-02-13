b5c665a4f2f23d6ca78fbc1ee8c4363011ece2d4
Narayanaswamy-m <venkatanarayanamedikonda@gmail.com>
2026-02-13 15:26:42 +0530

    changed something

---
+++ variable.sh
@@ -0,0 +1,7 @@
+#!/bin/bash 
+Person1=Rajesh;
+Person2=ramesh;
+echo "$Person1:: Hi $Person2 How are you?"
+echo "$person2 :: iam good how about you $Person1 ?"
+echo "$Person1 :: iam good thanks for asking $person2 are you free now ?"
+echo "$person2 :: yes iam free"
