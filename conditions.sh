1faeae0bd422b563b25e834ab585d32fd446ea88
Narayanaswamy-m <venkatanarayanamedikonda@gmail.com>
2026-02-16 09:39:37 +0530

    changed something

--- conditions.sh
+++ conditions.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 user_id=$(id -u)
-if [$user_id -ne 0]; then 
+if [ $user_id -ne 0 ]; then 
 echo "please run this script with root access"
 exit 1 
 fi 
 # installing ngnix 
 dnf install ngnix
-if [$? -ne 0]; then 
+if [ $? -ne 0 ]; then 
 echo "ngnix installation status failure ....."
 exit 1 
 else 
