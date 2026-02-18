b76160c4cfd4adc79ca5183c7c2c78aed1c63d61
Narayanaswamy-m <venkatanarayanamedikonda@gmail.com>
2026-02-18 09:54:29 +0530

    changed something

---
+++ loops.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+userid=$(id -u)
+Logs_folder="var/log/Devops-Projects2026"
+Logs_filename="var/log/Devops-Projects2026/$0.log"
+if [ $userid -ne 0 ]; then 
+echo "please run this script with root access"
+exit 1 
+fi 
+mkdir -p $Logs_folder;
+validate () {
+    if [ $1 -ne 0 ]; then 
+echo "$2 installation status failure ....." | tee -a $Logs_filename
+exit 1 
+else 
+echo "$2 installation status success...." | tee -a $Logs_filename
+fi 
+
+}
+
+for package in $@ 
+do 
+dnf install $package -y &>> $Logs_filename  
+validate $?  "$package Installing  ...." 
+done 
