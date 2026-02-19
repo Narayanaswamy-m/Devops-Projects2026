abee4bee2ec4a957ca24078dd50aedb27f11302e
Narayanaswamy-m <venkatanarayanamedikonda@gmail.com>
2026-02-19 10:25:24 +0530

    changed something

--- diskspace.sh
+++ diskspace.sh
@@ -2,8 +2,8 @@
 set -e 
 trap 'echo "there is an error $LINENO, COmmand:$BASH_COMMAND"' ERR
 Diskuseage=$(df -hT | awk 'NR==1 {print $1,$5}')
-Thresholdvalue="80"
-if [ $Diskuseage -gt $Thresholdvalue ]; then 
+Thresholdvalue=80
+if [ "$Diskuseage" -gt "$Thresholdvalue" ]; then 
 echo " Diskuseage $Diskuseage is exceeding $Thresholdvalue it is almost full"
 else 
 echo "Diskuseage value is not full"
