562e818d6f428d77a0b00fbd84d7b8302768551d
Narayanaswamy-m <venkatanarayanamedikonda@gmail.com>
2026-02-19 09:46:51 +0530

    changed something

--- trap.sh
+++ trap.sh
@@ -1,5 +1,5 @@
 #!/bin/bash 
 set -e #ERR
-trap ' echo "There is an error $LINENO, command:$BASH-Command"' ERR
+trap ' echo "There is an error $LINENO, command:$BASH_COMMAND"' ERR
 echo " Hellow ===="
 echooo " this  is hary....." 
